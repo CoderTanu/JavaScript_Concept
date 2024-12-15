29a9b4d601d225d0e5fd0b18b096a7a11a076f21
Tanu Kumari <tk6353129@gmail.com>
2024-12-15 18:56:30 +0530

    js

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function hide(){
+  document.getElementById('content').style.display ='none';
+}
+
+function Show(){
+document.getElementById('content').style.display ='block';
+}
+
+function turnOn(){
+    document.getElementById('image').src ='../images/pic_bulbon.gif';
+}
+
+
+function turnOff(){
+    document.getElementById('image').src ='../images/pic_bulboff.gif';
+}
+
+function clickMe(){
+    document.getElementById('demo').style.backgroundColor ='yellow';
+}
